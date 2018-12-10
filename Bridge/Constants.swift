4158a25b8a7a74a188f4f953b2e97abb45138105
User <User@2600-6c5e-2900-0178-0000-aabd-c5af-1dcf.dhcp6.chtrptr.net>
2018-12-10 01:36:04 -0500

    .Enhancement: Added a new field to the settings bundle that can be changed in the settings, and then seen through a label within the app.

---
+++ Bridge/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  Bridge
+//
+//  Created by User on 12/10/18.
+//  Copyright © 2018 witschgerrj. All rights reserved.
+//
+
+import Foundation
+
+let officerKey = "officer"
+let authorizationCodeKey = "authorizationCode"
+let rankKey = "rank"
+let warpDriveKey = "warp"
+let warpFactorKey = "warpFactor"
+let favoriteTeaKey = "favoriteTea"
+let favoriteCaptainKey = "favoriteCaptain"
+let favoriteGadgetKey = "favoriteGadget"
+let favoriteAlienKey = "favoriteAlien"
+let ownerNameKey = "ownerName"
--- Bridge/SecondViewController.swift
+++ Bridge/SecondViewController.swift
@@ -9,17 +9,66 @@
 import UIKit
 
 class SecondViewController: UIViewController {
-
+    
+    @IBOutlet weak var engineSwitch: UISwitch!
+    @IBOutlet weak var warpFactorSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @objc func applicationWillEnterForeground(notification:NSNotification) {
+        let defaults = UserDefaults.standard
+        defaults.synchronize()
+        refreshFields()
+    }
+    
+    @IBAction func onWarpSliderDragged(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        defaults.set(warpFactorSlider.value, forKey: warpFactorKey)
+        defaults.synchronize()
+    }
+    
+    @IBAction func onSettingsButtonTapped(_ sender: AnyObject) {
+        let application = UIApplication.shared
+        let url = URL(string: UIApplicationOpenSettingsURLString)! as URL
+        if application.canOpenURL(url) {
+            application.open(url, options:["":""] , completionHandler: nil)
+        }
+    }
+    
+    @IBAction func onEngineSwitchTapped(_ sender: AnyObject) {
+        let defaults = UserDefaults.standard
+        defaults.set(engineSwitch.isOn, forKey: warpDriveKey)
+        defaults.synchronize()
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        refreshFields()
+        let app = UIApplication.shared
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.applicationWillEnterForeground(notification:)),
+                                               name: Notification.Name.UIApplicationWillEnterForeground,
+                                               object: app)
+    }
+    
+    
+    func refreshFields() {
+        let defaults = UserDefaults.standard
+        engineSwitch.isOn = defaults.bool(forKey: warpDriveKey)
+        warpFactorSlider.value = defaults.float(forKey: warpFactorKey)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
